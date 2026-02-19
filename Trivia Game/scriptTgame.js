5c7a6cbf79f346c761709830fe1127c67da0ac02
jrcaringo <jayclemente.aringo@gmail.com>
2026-02-19 08:33:39 +0000

    update trivia game

--- Trivia Game/scriptTgame.js
+++ Trivia Game/scriptTgame.js
@@ -1,27 +1,32 @@
-const question = "What planet is known as the 'Red Planet'?";
-const choices = ["Earth", "Mars", "Jupiter", "Saturn"];
-const correctAnswer = "Mars";
+const questions = ["What planet is known as the 'Red Planet'?", "Who painted the Mona Lisa"];
+let currentQuestionIndex = 0;
+const choicesArray = [["Earth", "Mars", "Jupiter", "Saturn"],["Van Gogh", "Picasso", "Da Vinci", "Rembrandt"]];
+const correctAnswers = ["Mars", "Da Vinci"];
+let score = 0;
 
 function displayQuestion(){
-  document.getElementById("question").innerHTML = question;
+  if (currentQuestionIndex < questions.length){
+    document.getElementById("question").innerHTML = questions[currentQuestionIndex];
 
-  for(let i = 0; i < 4; i++){
-    const btn = document.getElementById(`choice${i+1}`);
-    btn.innerHTML = choices[i];
-    btn.value = choices[i];
+    for(let i = 0; i < 4; i++){
+      const btn = document.getElementById(`choice${i+1}`);
+      btn.innerHTML = choicesArray[currentQuestionIndex][i];
+      btn.value =choicesArray[currentQuestionIndex][i];
+    }
+  }
+  else{
+    document.getElementById("result").innerHTML = `You scored ${score} out of ${questions.length}!`;
+    document.getElementById("question").innerHTML = "";
+    document.getElementById("choices").innerHTML = "";
   }
 }
 
 function checkAnswer(button){
-  if (button.value === correctAnswer){
-    document.getElementById("result").innerHTML = "Correct!";
+  if (button.value === correctAnswers[currentQuestionIndex]) {
+    score++;
   }
-  else{
-    document.getElementById("result").innerHTML = "Wrong!";
-  }
-  for(let i = 0; i < 4; i++){
-    document.getElementById(`choice${i+1}`).disabled = true;
-  }
+  currentQuestionIndex++;
+  displayQuestion();
 }
 
 displayQuestion();
